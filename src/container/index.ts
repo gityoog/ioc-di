2b98d1867d51099c59ad4dbceae7f5f80845766e
yoog <907850084@qq.com>
2021-06-08 09:49:04 +0800

    增加了容器销毁方法 增加了子容器 增加了dataSet

--- src/container/index.ts
+++ src/container/index.ts
@@ -1,13 +1,17 @@
+import InstanceMeta from "../instance-meta"
 import Token from "../token"
 
 export default class Container {
   dataMap = new WeakMap<Token, any>()
   resolverMap = new WeakMap<Token, () => any>()
-
+  dataSet = new Set<any>()
   parent?: this
   link(parent: this) {
+    parent.children.add(this)
     this.parent = parent
   }
+
+  children = new Set<this>()
 
   register<T>(key: any, resolver: () => T) {
     this.resolverMap.set(Token.Create(key), resolver)
@@ -53,10 +57,28 @@ export default class Container {
   setData<T>(token: Token, data: T) {
     if (!this.dataMap.has(token)) {
       this.dataMap.set(token, data)
+      this.addData(data)
     } else {
       if (this.dataMap.get(token) !== data) {
         throw new Error('Different values ​​in the container')
       }
     }
+  }
+
+  addData<T>(data: T) {
+    this.dataSet.add(data)
+    return data
+  }
+
+  destroy() {
+    this.children.forEach(container => {
+      container.destroy()
+    })
+    this.dataSet.forEach(data => {
+      InstanceMeta.Get(data)?.destroy()
+    })
+    this.parent = null!
+    this.dataSet.clear()
+    this.children.clear()
   }
 }
